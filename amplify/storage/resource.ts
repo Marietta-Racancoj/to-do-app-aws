2d29be12011b8e550d0fd65f681ef76f55fd4f26
Marietta Racancoj <s1064730@monmouth.edu>
2025-05-06 11:29:25 -0400

    fix

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -3,22 +3,11 @@ import { defineStorage } from '@aws-amplify/backend';
 export const storage = defineStorage({
   name: 'to-do-s3-bucket-admin-amplify',
   access: (allow) => ({
-    'public/*': [
+    'protected/*': [
       allow.authenticated.to(['get', 'list', 'write', 'delete']),
     ],
   }),
 });
 
 
-
-
-// import { defineStorage } from '@aws-amplify/backend';
-
-// export const storage = defineStorage({
-//   name: "to-do-s3-bucket-admin-amplify",
-//   access: (allow) => ({
-//     // Authenticated users can read & write
-//     'photos/*': [allow.authenticated.to(['read', 'write','delete'])],
-//   }),
-// });
 
